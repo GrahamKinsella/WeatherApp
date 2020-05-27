6622f986ff9eb0753741ee6f0bfe6ba6671391ec
graham kinsella <graham_kinsella3@hotmail.com>
2020-05-27 21:02:50 +0100

    Add project files.

---
+++ Backend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Backend.Helpers;
+using Backend.Objects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+        public WeatherForecastController(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+            Map map = new Map(_configuration);
+            var location = "Dublin, Ireland";
+            Coordinates coordinates = map.GetLatAndLong(location);
+
+            string key = _configuration["OpenWeatherApiKey"];
+            var request = $"https://api.openweathermap.org/data/2.5/onecall?lat={coordinates.Lat}&lon={coordinates.Long}&exclude=hourly,daily&appid={key}";
+            var client = _clientFactory.CreateClient();
+            var response = await client.GetAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            //TODO: Create object to deserialize to
+            var json = JsonConvert.DeserializeObject(responseBody);
+
+        }
+    }
+}
---
+++ Backend/Helpers/Map.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Objects;
+using GoogleMaps.LocationServices;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Helpers
+{
+    public class Map
+    {
+        private readonly IConfiguration _configuration;
+        public Map(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Coordinates GetLatAndLong(string location)
+        {
+            var address = new AddressData // Belgium
+            {
+                City = "DUBLIN",
+                State = null,
+                Country = "IRELAND",
+                Zip = "D05Y996"
+            };
+
+            var key = _configuration["GoogleApiKey"];
+            var locationService = new GoogleLocationService(apikey: key);
+
+            var point = locationService.GetLatLongFromAddress(address);
+
+            Coordinates co = new Coordinates()
+            {
+                Lat = point.Latitude.ToString(),
+                Long = point.Longitude.ToString()
+            };
+
+            return co;
+        }
+    }
+}
+
